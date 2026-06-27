146652f401b4e2d5566cd7ba2c769694b261a23e
saiaungzawoo <saimon39999@gmail.com>
2026-06-27 16:02:42 +0630

    fix

--- src/main/java/com/finalproject/Final/controller/CourseController.java
+++ src/main/java/com/finalproject/Final/controller/CourseController.java
@@ -1,4 +1,4 @@
-package com.finalproject.Final.controller;
+package com.finalproject.Final.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
